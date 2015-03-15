fac88ee8ca9477ddf027c4b2b2ac30f25412f62d
Arman Kamal <arman1790@gmail.com>
2015-03-15 23:26:39 +0600

    let's work section

--- public/js/template.js
+++ public/js/template.js
@@ -10,7 +10,7 @@
 (function($){
 	$(document).ready(function(){
 	
-		$(".banner-image").backstretch('images/banner.jpg');
+		$(".banner-image").backstretch('images/jpt.jpg');
 		
 		// Fixed header
 		//-----------------------------------------------
